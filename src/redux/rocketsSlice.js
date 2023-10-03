6a3e1c6c8bc2e3e91e5336e0eac3bea630dfe424
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-03 15:17:24 +0300

    Add rockets redux slice

---
+++ src/redux/rocketsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState: [],
+  reducers: {},
+});
+
+export default rocketsSlice.reducer;
