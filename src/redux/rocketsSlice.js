4d7ff1585ecff759fb6b9b79a1142d1585c44cb7
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-04 16:42:11 +0300

    Fetch rockets and add extra reducers

--- src/redux/rocketsSlice.js
+++ src/redux/rocketsSlice.js
@@ -1,9 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const URL = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async (_, { rejectWithValue }) => axios.get(URL).then((response) => response.data.map(({
+  id, rocket_name: name, rocket_type: type, flickr_images: images,
+}) => ({
+  id, name, type, images,
+}))).catch((error) => rejectWithValue(error.message)));
+
+const initialState = {
+  loading: false,
+  rockets: [],
+  error: '',
+};
 
 const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState: [],
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => ({ ...state, loading: true }))
+      .addCase(fetchRockets.fulfilled, (state, action) => (
+        { ...state, rockets: action.payload, loading: false }))
+      .addCase(fetchRockets.rejected, (state, action) => (
+        { ...state, error: action.payload, loading: false }));
+  },
 });
 
 export default rocketsSlice.reducer;
