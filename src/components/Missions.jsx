d29a363722b41c6bf213342fe22cdc4d4218108a
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-04 14:19:15 +0300

    Fetch mission data from spaceX API and dispatch an action to store the selected data in Redux store

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,24 @@
-const Missions = () => (
-  <div>Missions</div>
-);
+/** @format */
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missionsSlice';
+
+const Missions = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, []);
+
+  return (
+    <div>
+      {missions.map((mission) => (
+        <pre key={mission.missionId}>{mission.missionName}</pre>
+      ))}
+    </div>
+  );
+};
 
 export default Missions;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionsReducer from './redux/missionsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    missions: missionsReducer,
+  },
 });
 
 export default store;
