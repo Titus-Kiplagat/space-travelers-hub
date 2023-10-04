767f928961f12aa4adf65ef1d36bb8436f210760
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-04 16:43:27 +0300

    Dispatch action to store rockets in the redux store

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,5 +1,34 @@
-const Rockets = () => (
-  <div className="mt-5">Rockets</div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rocketsSlice';
+
+const Rockets = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Rockets</h1>
+      <ul>
+        {rockets.map((rocket) => (
+          <li key={rocket.id}>
+            <strong>Name:</strong>
+            {' '}
+            {rocket.name}
+            ,
+            {' '}
+            <strong>Type:</strong>
+            {' '}
+            {rocket.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
