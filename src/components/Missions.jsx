7a669f4188560e59709f4be761588093351d74e4
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-04 18:09:12 +0300

    Display missions on missions route page

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,3 +1,6 @@
+/** @format */
+
+import { Table, Button } from 'react-bootstrap';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../redux/missionsSlice';
@@ -11,11 +14,27 @@ const Missions = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      {missions.map((mission) => (
-        <pre key={mission.missionId}>{mission.missionName}</pre>
-      ))}
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th colSpan={4}>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map(({ missionId, missionName, description }) => (
+          <tr key={missionId}>
+            <td>{missionName}</td>
+            <td>{description}</td>
+            <div className="buttons">
+              <Button variant="secondary">NOT A MEMBER</Button>
+              <Button variant="light">JOIN MISSION</Button>
+            </div>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
