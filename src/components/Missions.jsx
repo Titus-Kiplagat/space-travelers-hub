3b5387ed7113727d44270a9b7113f932982326ea
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-05 18:37:05 +0300

    Adjust width of the button and table

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -24,57 +24,57 @@ const Missions = () => {
     return <h1 className="container align-middle text-center">{error}</h1>;
   }
   return (
-    <Table striped className="container">
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th colSpan={4}>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {missions.map(({
-          missionId, missionName, description, reserved,
-        }) => (
-          <tr key={missionId}>
-            <td className="bold">{missionName}</td>
-            <td>{description}</td>
-            <td className="align-middle text-center">
-              {reserved ? (
-                <Badge className="bold bg-primary" variant="secondary">
-                  Active Member
-                </Badge>
-              ) : (
-                <Badge className="bold bg-secondary" variant="secondary">
-                  NOT A MEMBER
-                </Badge>
-              )}
-            </td>
-            <td className="align-middle text-center">
-              {reserved ? (
-                <Button
-                  size="lg"
-                  className="w-100 btn-outline-danger border-2 bold"
-                  variant="light"
-                  onClick={() => dispatch(leaveMission(missionId))}
-                >
-                  Leave Mission
-                </Button>
-              ) : (
-                <Button
-                  size="lg"
-                  className="w-100 btn-outline-secondary border-2 bold"
-                  variant="light"
-                  onClick={() => dispatch(joinMission(missionId))}
-                >
-                  Join Mission
-                </Button>
-              )}
-            </td>
+    <div className="px-5">
+      <Table striped bordered className="container-fluid">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th colSpan={4}>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {missions.map(({
+            missionId, missionName, description, reserved,
+          }) => (
+            <tr key={missionId}>
+              <td className="bold">{missionName}</td>
+              <td colSpan={9}>{description}</td>
+              <td className="align-middle text-center">
+                {reserved ? (
+                  <Badge className="bold bg-primary" variant="secondary">
+                    Active Member
+                  </Badge>
+                ) : (
+                  <Badge className="bold bg-secondary" variant="secondary">
+                    NOT A MEMBER
+                  </Badge>
+                )}
+              </td>
+              <td className="align-middle">
+                {reserved ? (
+                  <Button
+                    className="btn btn-outline-danger border-2 bold max-content-width"
+                    variant="light"
+                    onClick={() => dispatch(leaveMission(missionId))}
+                  >
+                    Leave Mission
+                  </Button>
+                ) : (
+                  <Button
+                    className="btn btn-outline-secondary border-2 bold max-content-width"
+                    variant="light"
+                    onClick={() => dispatch(joinMission(missionId))}
+                  >
+                    Join Mission
+                  </Button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
