a2a99286121764a71910c3d53e19d93c3c836c1c
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-05 15:01:27 +0300

    Create markup to display reserved rockets on the page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,21 @@
-const Profile = () => (
-  <div>
-    Profile
-  </div>
-);
+import { useSelector } from 'react-redux';
+import { ListGroup } from 'react-bootstrap';
+import RocketListItem from './RocketListItem';
+
+const Profile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  return (
+    <div className="px-5">
+      <h2 className="font-bold">Reserved Rockets</h2>
+      {reservedRockets.length === 0 && <p className="fs-2">You have no reserved rockets</p>}
+      <ListGroup className="my-2">
+        {reservedRockets.map((rocket) => (
+          <RocketListItem key={rocket.id} rocket={rocket} />
+        ))}
+      </ListGroup>
+    </div>
+  );
+};
 
 export default Profile;
