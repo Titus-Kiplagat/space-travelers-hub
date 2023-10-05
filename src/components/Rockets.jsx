4aa918e081bec9a2c9bf163ed51909617e278a70
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-05 14:56:22 +0300

    Add child rocket list item component and passed data as props

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable no-console */
+/* eslint-disable max-len */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Button, Card, Stack, Container, Badge,
-} from 'react-bootstrap';
-import { cancelRocket, fetchRockets, reserveRocket } from '../redux/rocketsSlice';
+import { ListGroup } from 'react-bootstrap';
+import { fetchRockets } from '../redux/rocketsSlice';
+import RocketListItem from './RocketListItem';
 
 const Rockets = () => {
   const { rockets } = useSelector((state) => state.rockets);
@@ -15,30 +14,13 @@ const Rockets = () => {
   }, [dispatch]);
 
   return (
-    <>
+    <ListGroup className="my-2 px-5">
       {
-      rockets.map(({
-        id, name, images, description, reserved,
-      }) => (
-        <Card key={id} className="mb-3 border-0">
-          <Container fluid>
-            <Stack direction="horizontal" gap={3}>
-              <Card.Img variant="top" src={images[0]} className="custom-width" />
-              <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                  {reserved && <Badge className="me-2">Reserved</Badge>}
-                  {description}
-                </Card.Text>
-                {reserved ? (<Button onClick={() => dispatch(cancelRocket(id))} variant="outline-secondary">Cancel Reservation</Button>)
-                  : (<Button onClick={() => dispatch(reserveRocket(id))} variant="primary">Reserve Rocket</Button>)}
-              </Card.Body>
-            </Stack>
-          </Container>
-        </Card>
-      ))
-}
-    </>
+        rockets.map((rocket) => (
+          <RocketListItem key={rocket.id} rocket={rocket} />
+        ))
+      }
+    </ListGroup>
   );
 };
 
