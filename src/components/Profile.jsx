4578db4b1389242fd86263c964938615a17593d5
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-05 18:36:22 +0300

    Add padding to match the design layout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -13,24 +13,24 @@ const Profile = () => {
   return (
     <div className="px-5 d-flex justify-content-between gap-5">
       <div className="w-100">
-        <h2 className="font-bold">My Missions</h2>
+        <h3 className="font-bold py-2">My Missions</h3>
         {reservedRockets.length === 0 && (
           <p className="fs-2">You have no reserved rockets</p>
         )}
         <ListGroup className="my-2">
           {reservedMissions.map(({ missionName, missionId }) => (
-            <ListGroup.Item key={missionId}>{missionName}</ListGroup.Item>
+            <ListGroup.Item className="py-4" key={missionId}>{missionName}</ListGroup.Item>
           ))}
         </ListGroup>
       </div>
       <div className="w-100">
-        <h2 className="font-bold">My Rockets</h2>
+        <h3 className="font-bold py-2">My Rockets</h3>
         {reservedRockets.length === 0 && (
           <p className="fs-2">You have no reserved rockets</p>
         )}
         <ListGroup className="my-2">
           {reservedRockets.map(({ name, id }) => (
-            <ListGroup.Item key={id}>{name}</ListGroup.Item>
+            <ListGroup.Item className="py-4" key={id}>{name}</ListGroup.Item>
           ))}
         </ListGroup>
       </div>
